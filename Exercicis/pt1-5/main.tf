eff7ca73afea49533afc49162b242c8494024efe
Frederick <vargasfred04@gmail.com>
2025-11-12 13:46:39 +0100

    pt1.5 grupo de seguridad

--- Exercicis/pt1-5/main.tf
+++ Exercicis/pt1-5/main.tf
@@ -35,3 +35,71 @@ resource "aws_internet_gateway" "igw" {
       Name = "${var.project_name}-igw"
     }
 }
+
+# tabla de rutas de subredes publicas
+resource "aws_route_table" "publica" {
+  vpc_id = aws_vpc.main.id 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    Gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+# Asociar cada subnet publica
+resource "aws_route_table_association" "public_subnets" {
+  count          = var.subnet_count
+  subnet_id      = aws_submet.public[count.index].id
+  route_table_id = aws_route_table.public.id 
+}
+
+#grupo de seguridad
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Security Group per a instàncies EC2"
+  vpc_id      = aws_vpc.main.id
+   
+  # Permet HTTP des de qualsevol IP
+  ingress {
+    description = "Permet HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Permet SSH només des de la IP definida (institut o casa)
+  ingress {
+    description = "Permet SSH des de IP definida"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  # Permet ICMP només des de dins la VPC
+  ingress {
+    description = "Permet ICMP només des de la VPC"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  # Permet tot el tràfic sortint
+  egress {
+    description = "Permet tot el tràfic sortint"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
